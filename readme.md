d09ea25d66beeb22a1f0fa93707e5dcd82c89ec1
alberto scionti <alberto.scionti@linksfoundation.com>
2024-05-08 09:52:08 +0200

    minor update 2024-05-08

---
+++ readme.md
@@ -0,0 +1,4 @@
+# KRONOS -- A Time-Sensitive Network Simulation Tool
+---
+
+**Kronos** is the an open source project that finds its roots in the need of precisely simulating Time-Sensitive Networks (TSNs), with a good degree of control on the simulated hardware (input FIFO queues, switching fabric, packet tagging, etc.). It is an ongoing project, thus it is very probable that new versions and bug fixes become available frequently.  
---
+++ src/kronos.c
@@ -0,0 +1,31 @@
+/****************************************************************
+ *                                                              *
+ *                         K R O N O S                          *
+ *                                                              *
+ *          2024 - 20XX LINKS Foundation (CPE Group)            *
+ *                            ----                              *
+ *                                                              *
+ *  Authors: A. Scionti                                         *
+ *  Version: 0.1                                                *
+ *  License: xyz                                                *
+ *                                                              *
+ ****************************************************************/
+
+// Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+
+// Symbolic constants
+#define SUCCESS		0
+#define FAILURE		1
+
+
+// Main function (entry point)
+int main(int argc, char** argv)
+{
+	//code
+	return (SUCCESS);
+}
